523998a530b7453dcc8b08ff76bdaf223a9ed3c5
Your Name <44114870+JdavisBro@users.noreply.github.com>
2024-08-12 01:49:02 +0100

    add markers, collectable list.

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,18 @@ import {
   LayerGroup,
   LayersControl,
   MapContainer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvents,
 } from "react-leaflet";
 
-import { Levels } from "./data/data";
+import { Animals, Gift, Gifts, Levels, Litter, OneLitter } from "./data/data";
 import { useState } from "react";
 import Control from "react-leaflet-custom-control";
 
-const SCREEN_SIZE = { x: 192, y: 108 };
+const SCALE = 0.1;
+const SCREEN_SIZE = { x: 1929 * SCALE, y: 1080 * SCALE };
 
 const QUALITIES = ["192x108", "384x216", "960x540", "1920x1080"];
 const QUALITY_SIZES: Record<string, string> = {
@@ -21,9 +26,136 @@ const QUALITY_SIZES: Record<string, string> = {
   "1920x1080": "223 MB",
 };
 
+enum Collectable {
+  None,
+  Litter,
+  Gift,
+  Animal,
+}
+
+function getPosition(screen: string, x: number, y: number) {
+  const [scr_x, scr_y] = screen
+    .split("_")
+    .map((i) => Number(i))
+    .slice(1);
+  return new L.LatLng(
+    -SCREEN_SIZE.y * scr_y - y * SCALE,
+    SCREEN_SIZE.x * scr_x + x * SCALE,
+  );
+}
+
+function filterMarkers(
+  obj: Record<string, { name?: string; screen: string; x: number; y: number }>,
+  layer: number,
+  markerIcon:
+    | string
+    | ((collectable: { screen: string; x: number; y: number }) => string),
+) {
+  return Object.keys(obj)
+    .filter((collId) => Number(obj[collId].screen[0]) == layer)
+    .map((collId) => (
+      <Marker
+        key={collId}
+        position={getPosition(obj[collId].screen, obj[collId].x, obj[collId].y)}
+        icon={L.icon({
+          iconUrl:
+            typeof markerIcon == "string"
+              ? markerIcon
+              : markerIcon(obj[collId]),
+          iconSize: [41, 41],
+          iconAnchor: [20, 25],
+        })}
+      >
+        {obj[collId].name ? (
+          <Popup offset={[0, -18]}>{obj[collId].name}</Popup>
+        ) : null}
+      </Marker>
+    ));
+}
+
+function MapEvents({ setLayer }: { setLayer: (layer: number) => void }) {
+  useMapEvents({
+    baselayerchange: (event) =>
+      setLayer(
+        event.name == "Surface" ? 0 : Number(event.name.replace("Layer ", "")),
+      ),
+  });
+
+  return null;
+}
+
+const CollectableMap: Record<
+  Collectable,
+  | undefined
+  | Record<string, { name?: string; screen: string; x: number; y: number }>
+> = {
+  [Collectable.None]: undefined,
+  [Collectable.Litter]: Litter,
+  [Collectable.Gift]: Gifts,
+  [Collectable.Animal]: Animals,
+};
+
+function CollectableList({
+  collectable,
+  layer,
+  setLayer,
+}: {
+  collectable: Collectable;
+  layer: number;
+  setLayer: (layer: number) => void;
+}) {
+  const map = useMap();
+  const obj = CollectableMap[collectable];
+  if (!obj) {
+    return null;
+  }
+  return Object.keys(obj).map((collId) => (
+    <a
+      href="javascript:void"
+      onClick={() => {
+        const coll = obj[collId];
+        const newLayer = Number(coll.screen[0]);
+        if (layer != newLayer) {
+          setLayer(newLayer);
+        }
+        const pos = getPosition(coll.screen, coll.x, coll.y);
+        map.flyTo(pos);
+        setTimeout(
+          () =>
+            map.eachLayer((mapLayer) => {
+              "getLatLng" in mapLayer &&
+                (mapLayer.getLatLng as () => L.LatLng)().equals(pos) &&
+                mapLayer.openPopup();
+            }),
+          100,
+        );
+      }}
+    >
+      {obj[collId].name ?? collId}
+    </a>
+  ));
+}
+
 export default function App() {
   const [quality, setQuality] = useState("384x216");
-  console.log(quality);
+
+  const [collectable, setCollectable] = useState(Collectable.None);
+  const toggleCollectable = (type: Collectable) =>
+    setCollectable(collectable == type ? Collectable.None : type);
+
+  const [layer, setLayer] = useState(0);
+
+  const litterMarkers = filterMarkers(Litter, layer, (litter: OneLitter) =>
+    litter.npc ? "markers/litterNPC.png" : "markers/litter.png",
+  );
+  const giftMarkers = filterMarkers(Gifts, layer, (gift: Partial<Gift>) =>
+    gift.npc
+      ? "markers/giftsNPC.png"
+      : gift.treasure
+        ? "markers/giftsTreasure.png"
+        : "markers/gifts.png",
+  );
+  const animalsMarkers = filterMarkers(Animals, layer, "markers/animal.png");
 
   return (
     <>
@@ -37,17 +169,20 @@ export default function App() {
         zoomAnimation={false}
         crs={L.CRS.Simple}
       >
+        <MapEvents setLayer={setLayer} />
         <LayersControl>
-          {Object.keys(Levels).map((layer) => (
+          {Object.keys(Levels).map((mappedLayer) => (
             <LayersControl.BaseLayer
-              checked={Number(layer) == 0}
-              name={Number(layer) == 0 ? "Surface" : `Layer ${layer}`}
+              checked={Number(mappedLayer) == layer}
+              name={
+                Number(mappedLayer) == 0 ? "Surface" : `Layer ${mappedLayer}`
+              }
             >
               <LayerGroup>
-                {Levels[layer].map((xy) => (
+                {Levels[mappedLayer].map((xy) => (
                   <ImageOverlay
                     key={xy.join("_")}
-                    url={`screens/${quality}/${layer}_${xy.join("_")}.png`}
+                    url={`screens/${quality}/${mappedLayer}_${xy.join("_")}.png`}
                     bounds={
                       new L.LatLngBounds(
                         [-SCREEN_SIZE.y * xy[1], SCREEN_SIZE.x * xy[0]],
@@ -62,9 +197,18 @@ export default function App() {
               </LayerGroup>
             </LayersControl.BaseLayer>
           ))}
+          <LayersControl.Overlay name="Litter" checked={true}>
+            <LayerGroup>{litterMarkers}</LayerGroup>
+          </LayersControl.Overlay>
+          <LayersControl.Overlay name="Gifts" checked={true}>
+            <LayerGroup>{giftMarkers}</LayerGroup>
+          </LayersControl.Overlay>
+          <LayersControl.Overlay name="Animals" checked={true}>
+            <LayerGroup>{animalsMarkers}</LayerGroup>
+          </LayersControl.Overlay>
         </LayersControl>
         <Control position="topright">
-          <a className="control-popup">✨</a>
+          <a className="control-popup control-popup-monochrome">✨</a>
           <section className="control-content">
             {QUALITIES.map((qual) => (
               <label>
@@ -79,6 +223,41 @@ export default function App() {
               </label>
             ))}
           </section>
+        </Control>
+        <Control position="bottomleft">
+          <a
+            className={`control-popup control-popup-keep${collectable == Collectable.Litter ? " control-popup-selected" : ""}`}
+            onClick={() => toggleCollectable(Collectable.Litter)}
+          >
+            🗑️
+          </a>
+        </Control>
+        <Control position="bottomleft">
+          <a
+            className={`control-popup control-popup-keep${collectable == Collectable.Gift ? " control-popup-selected" : ""}`}
+            onClick={() => toggleCollectable(Collectable.Gift)}
+          >
+            🎁
+          </a>
+        </Control>
+        <Control position="bottomleft">
+          <a
+            className={`control-popup control-popup-keep${collectable == Collectable.Animal ? " control-popup-selected" : ""}`}
+            onClick={() => toggleCollectable(Collectable.Animal)}
+          >
+            🐱
+          </a>
+        </Control>
+        <Control position="bottomleft">
+          <div
+            className={`collectable-list${collectable == Collectable.None ? " collectable-list-hidden" : ""}`}
+          >
+            <CollectableList
+              collectable={collectable}
+              layer={layer}
+              setLayer={setLayer}
+            />
+          </div>
         </Control>
       </MapContainer>
     </>
--- src/data/data.ts
+++ src/data/data.ts
@@ -4,26 +4,33 @@ import gifts from "./gifts.json";
 import litter from "./litter.json";
 import levels from "./levels.json";
 
-export const Animals: {
-  [key: string]: { screen: string; x: number; y: number };
-} = animals;
+export type Animal = { screen: string; x: number; y: number };
 
-export const Entrances: {
-  [key: string]: {
-    direction: string | null;
-    screen: string;
-    x: number;
-    y: number;
-    to: string | null;
-  };
-} = entrances;
+export const Animals: Record<string, Animal> = animals;
 
-export const Gifts: {
-  [key: string]: { name: string; screen: string; x: number; y: number };
-} = gifts;
+export type Entrance = {
+  direction: string | null;
+  screen: string;
+  x: number;
+  y: number;
+  to: string | null;
+};
 
-export const Litter: {
-  [key: string]: { screen: string; x: number; y: number };
-} = litter;
+export const Entrances: Record<string, Entrance> = entrances;
+
+export type Gift = {
+  name: string;
+  screen: string;
+  x: number;
+  y: number;
+  npc?: boolean;
+  treasure?: boolean;
+};
+
+export const Gifts: Record<string, Gift> = gifts;
+
+export type OneLitter = { screen: string; x: number; y: number; npc?: boolean };
+
+export const Litter: Record<string, OneLitter> = litter;
 
 export const Levels: Record<string, number[][]> = levels;
